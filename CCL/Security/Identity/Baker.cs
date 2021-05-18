f931341a8612424c7c5a07d4351363edaee4958e
AlyonaYashchenko <yalenka1707@gmail.com>
2021-05-18 21:25:55 +0300

    lab8

---
+++ CCL/Security/Identity/Baker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSBB.Security.Identity
+{
+    public class Baker
+        : Client
+    {
+        public Baker(int userId, string name, int catalogId) 
+            : base(userId, name, catalogId, nameof(Baker))
+        {
+        }
+    }
+}
